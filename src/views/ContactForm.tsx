432c1c256ae256d8f11c3e5164cc31a20fda72a2
Anbarasu <anbarasun123@gmail.com>
2022-12-26 02:55:58 +0000

    random changes

--- src/views/ContactForm.tsx
+++ src/views/ContactForm.tsx
@@ -1,23 +1,25 @@
 import { Heading, VStack, Text, FormControl, FormLabel, Input, FormErrorMessage, Flex, Textarea, Button, CheckboxGroup, Checkbox } from "@chakra-ui/react";
-import { useForm, Controller } from "react-hook-form";
+import { useForm, Controller, FormProvider, useFormContext } from "react-hook-form";
 import { InferType } from 'yup';
 import { formValidator } from "../validator";
 import axios from 'axios';
+import { yupResolver } from '@hookform/resolvers/yup';
+
 import qs from 'qs';
 
 type FormType = InferType<typeof formValidator>;
 
 export const ContactForm = () => {
-  const {
-    handleSubmit,
-    register,
-    control,
-    formState: { errors },
-  } = useForm<FormType>();
+  const methods = useForm<FormType>({ mode: 'all', resolver: yupResolver(formValidator) });
+
+  const { control, register, handleSubmit, formState: {
+    errors
+  } } = methods;
   const sendData = async (data: any) => {
+console.log(data);
     console.log(qs.stringify(data))
     const res = axios.post("https://mint-forms.ieee-mint.org/api/form/addresponse?formld-task", qs.stringify(data), {
-      headers: { 'content-type': 'application/x-www-form-urlencoded' },
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8' },
     }).then((re) => {
       console.log(re.data)
     })
@@ -33,85 +35,90 @@ export const ContactForm = () => {
       <Text textColor="blackAlpha.700">
         Your can reach us any time at untitledui@gmail.com
       </Text>
-      <form onSubmit={handleSubmit(sendData)} >
-        <FormControl>
-          <FormLabel htmlFor='name'>Name</FormLabel>
-          <Input
-            backgroundColor="white"
-            border="2px solid rgba(0,0,0,0.95)"
-            id='name'
-            placeholder='Name'
-            {...register('name')}
-          />
-          <FormErrorMessage>
-            {errors.name && errors.name.message}
-          </FormErrorMessage>
-        </FormControl>
-        <FormControl>
-          <FormLabel htmlFor='email'>Email</FormLabel>
-          <Input
-
-            backgroundColor="white"
-            border="2px solid rgba(0,0,0,0.95)"
+      <FormProvider {...methods}>
+        <form onSubmit={handleSubmit(sendData)} >
+          <FormControl width="350px">
+            <FormLabel htmlFor='name'>Name</FormLabel>
+            <Input
+              backgroundColor="white"
+              isRequired
+              border="2px solid rgba(0,0,0,0.95)"
+              id='name'
+              placeholder='Name'
+              {...register('name')}
+            />
+            <FormErrorMessage>
+              {errors.name && errors.name.message}
+            </FormErrorMessage>
+          </FormControl>
+          <FormControl
             width="350px"
-            id='email'
-            placeholder='Email'
-            {...register('email')}
-          />
-          <FormErrorMessage>
-            {errors.email && errors.email.message}
-          </FormErrorMessage>
-        </FormControl>
-        <FormControl>
-          <FormLabel htmlFor='phone'>Phone</FormLabel>
-          <Input
+          >
+            <FormLabel htmlFor='email'>Email</FormLabel>
+            <Input
+              isRequired
+              backgroundColor="white"
+              border="2px solid rgba(0,0,0,0.95)"
+              id='email'
+              placeholder='Email'
+              {...register('email')}
+            />
+            <FormErrorMessage>
+              {errors.email && errors.email.message}
+            </FormErrorMessage>
+          </FormControl>
+          <FormControl width="350px">
+            <FormLabel htmlFor='phone'>Phone</FormLabel>
+            <Input
+              isRequired
+              backgroundColor="white"
+              border="2px solid rgba(0,0,0,0.95)"
+              id='phone'
+              placeholder='phone'
+              {...register('phone')}
+            />
+            <FormErrorMessage>
+              {errors.phone && errors.phone.message}
+            </FormErrorMessage>
+          </FormControl>
+          <FormControl
+            width="350px">
+            <FormLabel htmlFor='description'>Description</FormLabel>
+            <Textarea
 
-            backgroundColor="white"
-            border="2px solid rgba(0,0,0,0.95)"
-            id='phone'
-            placeholder='phone'
-            {...register('phone')}
-          />
-          <FormErrorMessage>
-            {errors.phone && errors.phone.message}
-          </FormErrorMessage>
-        </FormControl>
-        <FormControl>
-          <FormLabel htmlFor='description'>Description</FormLabel>
-          <Textarea
-
-            backgroundColor="white"
-            border="2px solid rgba(0,0,0,0.95)"
-            id='description'
-            placeholder='description'
-            {...register('description')}
-          />
-          <FormErrorMessage>
-            {errors.description && errors.description.message}
-          </FormErrorMessage>
-        </FormControl>
-        <FormControl>
-          <Controller
-            name="services"
-            control={control}
-            render={({ field: { ref, ...rest } }) => (
-              <CheckboxGroup {...rest}>
-                <Flex marginBlock="10px" flexWrap="wrap" columnGap="10px" rowGap="10px">
-                  <Checkbox value="Website Design">Website Design</Checkbox>
-                  <Checkbox value="Content Creation">Content Creation</Checkbox>
-                  <Checkbox value="UX Design">UX Design</Checkbox>
-                  <Checkbox value="Strategy & Consulting">Strategy & Consulting</Checkbox>
-                  <Checkbox value="UX Research">UX Research</Checkbox>
-                  <Checkbox value="Other">Other</Checkbox>
-                </Flex>
-              </CheckboxGroup>
-            )}
-          />
-        </FormControl>
-        <Button textColor="white" background="#4A5364" marginTop="10px" width="350px" type="submit" >
-          Send Message
-        </Button>
-      </form>
+              backgroundColor="white"
+              border="2px solid rgba(0,0,0,0.95)"
+              id='description'
+              placeholder='description'
+              {...register('description')}
+            />
+            <FormErrorMessage>
+              {errors.description && errors.description.message}
+            </FormErrorMessage>
+          </FormControl>
+          <FormControl>
+            <Controller
+              name="services"
+              control={control}
+              render={({ field: { ref, ...rest } }) => (
+                <CheckboxGroup {...rest}>
+                  <Flex marginBlock="10px" flexWrap="wrap" columnGap="10px" rowGap="10px">
+                    <Checkbox value="Website Design">Website Design</Checkbox>
+                    <Checkbox value="Content Creation">Content Creation</Checkbox>
+                    <Checkbox value="UX Design">UX Design</Checkbox>
+                    <Checkbox value="Strategy & Consulting">Strategy & Consulting</Checkbox>
+                    <Checkbox value="UX Research">UX Research</Checkbox>
+                    <Checkbox value="Other">Other</Checkbox>
+                  </Flex>
+                </CheckboxGroup>
+              )}
+            />
+          </FormControl>
+          <Button textColor="white" background="#4A5364" marginTop="10px" width="350px" type="submit" >
+            Send Message
+          </Button>
+        </form>
+      </FormProvider>
 
     </VStack >
   );
